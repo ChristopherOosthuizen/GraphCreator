017bea86332c9367642b803ada94e1d43d542985
ChristopherOosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-06-04 17:06:55 -0700

    Fixed some errors in Benchmakrs

--- src/GraphCreation/BenchMarks.py
+++ src/GraphCreation/BenchMarks.py
@@ -9,6 +9,7 @@ import threading
 from . import GraphCreation as gc
 from . import LLMFunctions as lm
 from . import textformatting as tx
+from . import LinkPrediction as lp
 current_file_path = os.path.abspath(__file__)
 current_dir = os.path.dirname(current_file_path)
 prompts_dir = os.path.join(current_dir, '..', 'prompts')
